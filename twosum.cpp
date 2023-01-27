fe950c4ad178283ad29e57fb5f16d740fc8aba46
itsmht <iamtalha.mht@gmail.com>
2023-01-27 19:13:25 +0600

    two sum

---
+++ twosum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map <int, int> um;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(um.find(target-nums[i]) == um.end())
+                um[nums[i]] = i;
+            else
+            return {um[target-nums[i]], i};
+        }
+        return {-1, -1};
+    }
+};
